497029e77df74c6039ff43644fad2a6b95463a40
Meshmanuu <meshanyangu@gmail.com>
2024-03-24 23:40:24 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let thankYouMessages = [];
+    for (let name of names) {
+      thankYouMessages.push(`Thank you, ${name}, for the wonderful ${event} gift!`);
+    }
+    return thankYouMessages;
+  }
+  
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  module.exports = {
+    writeCards,
+    countDown
+  };
+  
